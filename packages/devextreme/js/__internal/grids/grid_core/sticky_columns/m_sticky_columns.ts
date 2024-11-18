10fd7176c208d31e7154c1853ccf83a0c8db9f8c
Alyar <Alyar666@users.noreply.github.com>
2024-11-18 11:00:08 +0400

    DataGrid: Fix incorrect position of validation message and revert button when scrolling (#28360)
    Co-authored-by: Alyar <>

--- packages/devextreme/js/__internal/grids/grid_core/sticky_columns/m_sticky_columns.ts
+++ packages/devextreme/js/__internal/grids/grid_core/sticky_columns/m_sticky_columns.ts
@@ -1,6 +1,7 @@
 /* eslint-disable max-classes-per-file */
 import type { dxElementWrapper } from '@js/core/renderer';
 import $ from '@js/core/renderer';
+import { getBoundingRect } from '@js/core/utils/position';
 import { setWidth } from '@js/core/utils/size';
 import type { EditorFactory } from '@ts/grids/grid_core/editor_factory/m_editor_factory';
 
@@ -483,19 +484,12 @@ const rowsView = (
 
     if (hasStickyColumns) {
       const editorFactoryController = this.getController('editorFactory');
-      const $focusOverlay = editorFactoryController.getFocusOverlay();
+      const hasOverlayElements = editorFactoryController.hasOverlayElements();
 
-      if (!$focusOverlay?.hasClass(CLASSES.hidden)
-        && $focusOverlay?.hasClass(CLASSES.focusedFixedCell)) {
-        const $element = this.component.$element();
-        // @ts-expect-error
-        const $focusedCell = $element.find(`.${CLASSES.focused}`);
-        const isStickyCell = GridCoreStickyColumnsDom
-          .isStickyCell($focusedCell, this.addWidgetPrefix.bind(this));
+      if (hasOverlayElements) {
+        const $focusedElement = editorFactoryController.focus();
 
-        if (isStickyCell) {
-          editorFactoryController.updateFocusOverlay($focusedCell);
-        }
+        editorFactoryController.focus($focusedElement);
       }
 
       if (hasFixedColumnsWithStickyPosition(this._columnsController)) {
@@ -681,25 +675,99 @@ const draggingHeader = (Base: ModuleType<DraggingHeaderViewController>) => class
 };
 
 const editorFactory = (Base: ModuleType<EditorFactory>) => class EditorFactoryStickyColumnsExtender extends Base {
-  public updateFocusOverlay($element: dxElementWrapper, isHideBorder = false): void {
+  private getOverlayContainerIfNeeded($cell: dxElementWrapper): dxElementWrapper | undefined {
+    // @ts-expect-error
+    const hasFixedColumns = this._rowsView.hasStickyColumns();
+    const isFixedCell = GridCoreStickyColumnsDom.isFixedCell(
+      $cell,
+      this.addWidgetPrefix.bind(this),
+    );
+
+    if (hasFixedColumns && isFixedCell) {
+      return $cell.closest(`.${this.addWidgetPrefix(CLASSES.stickyColumns)}`);
+    }
+
+    return undefined;
+  }
+
+  protected updateFocusOverlaySize($element, position): void {
+    // @ts-expect-error
+    const hasFixedColumns = this._rowsView.hasStickyColumns();
+
+    if (!hasFixedColumns) {
+      super.updateFocusOverlaySize($element, position);
+    }
+  }
+
+  protected getFocusOverlaySize($element: dxElementWrapper): { width: number; height: number } {
+    // @ts-expect-error
+    const hasFixedColumns = this._rowsView.hasStickyColumns();
+
+    if (hasFixedColumns) {
+      const elementRect = getBoundingRect($element.get(0));
+      const isLastCell = GridCoreStickyColumnsDom.isLastCell($element);
+      const isFixedCell = GridCoreStickyColumnsDom.isFixedCell(
+        $element,
+        this.addWidgetPrefix.bind(this),
+      );
+
+      return {
+        width: elementRect.right - elementRect.left + (isLastCell || isFixedCell ? 0 : 1),
+        height: elementRect.bottom - elementRect.top,
+      };
+    }
+
+    return super.getFocusOverlaySize($element);
+  }
+
+  protected getValidationMessageContainer($cell: dxElementWrapper): dxElementWrapper {
+    // @ts-expect-error
+    return this.getOverlayContainerIfNeeded($cell) ?? super.getValidationMessageContainer($cell);
+  }
+
+  protected getRevertButtonContainer($cell: dxElementWrapper): dxElementWrapper {
+    // @ts-expect-error
+    return this.getOverlayContainerIfNeeded($cell) ?? super.getRevertButtonContainer($cell);
+  }
+
+  protected getFocusOverlayContainer($focusedElement: dxElementWrapper): dxElementWrapper {
+    return this.getOverlayContainerIfNeeded($focusedElement)
+      ?? super.getFocusOverlayContainer($focusedElement);
+  }
+
+  protected overlayPositionedHandler(e, isOverlayVisible: boolean): void {
+    const columnHeaders = this.getView('columnHeadersView');
+    // @ts-expect-error
+    const hasStickyColumns = columnHeaders.hasStickyColumns();
+    // @ts-expect-error
+    super.overlayPositionedHandler(e, isOverlayVisible);
+
+    if (hasStickyColumns) {
+      const $cell = $(e.element).closest('td');
+
+      if (!GridCoreStickyColumnsDom.isFixedCell($cell, this.addWidgetPrefix.bind(this))) {
+        const $wrapper = e.component.$wrapper();
+        const $overlayContent = e.component.$content();
+        const isOutsideVisibleArea = GridCoreStickyColumnsDom
+          .isOutsideVisibleArea(
+            $overlayContent,
+            $(columnHeaders.getColumnElements()),
+            $(columnHeaders.getContent()),
+            this.addWidgetPrefix.bind(this),
+          );
+
+        // @ts-expect-error
+        $wrapper.css('zIndex', isOutsideVisibleArea ? 1 : this?.getOverlayBaseZIndex() ?? 0);
+      }
+    }
+  }
+
+  protected updateFocusOverlay($element: dxElementWrapper, isHideBorder = false): void {
     if (!isHideBorder) {
-      const scrollable = this._rowsView.getScrollable();
-      const $container = $(scrollable?.container());
       const isFixedCell = GridCoreStickyColumnsDom
         .isFixedCell($element, this.addWidgetPrefix.bind(this));
-      const isStickyCell = GridCoreStickyColumnsDom
-        .isStickyCell($element, this.addWidgetPrefix.bind(this));
-      const isStickyCellPinned = isStickyCell && $container.length
-        && GridCoreStickyColumnsDom
-          .isStickyCellPinned($element, $container, this.addWidgetPrefix.bind(this));
 
       this._$focusOverlay.toggleClass(CLASSES.focusedFixedCell, isFixedCell);
-
-      if (isFixedCell && (!isStickyCell || isStickyCellPinned)) {
-        this._$focusOverlay.css('position', 'fixed');
-      } else {
-        this._$focusOverlay.css('position', '');
-      }
     }
 
     super.updateFocusOverlay($element, isHideBorder);
